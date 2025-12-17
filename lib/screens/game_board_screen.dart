14ed10edfe6587370489282f201b7eb30b49c486
MoriumIslam <152001270+MoriumIslam@users.noreply.github.com>
2025-12-17 12:29:00 +0600

    game board my app

--- lib/screens/game_board_screen.dart
+++ lib/screens/game_board_screen.dart
@@ -1,3 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:demo/models/game_models.dart';
 import 'package:demo/services/game_service.dart';
+class GameBoardScreen extends StatefulWidget {
+  final Player player1;
+  final Player player2;
+
+  const GameBoardScreen({
+    Key? key,
+    required this.player1,
+    required this.player2,
+  }) : super(key: key);
