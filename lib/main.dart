504deeb0bbc6a53ae22a30846ea2f7db020eb81e
obidul huq sagor <72642687+odhsagor@users.noreply.github.com>
2025-12-17 16:13:03 +0600

    Main.dart Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,18 @@
+import 'package:demo/providers/game_provider.dart';
+import 'package:demo/providers/history_provider.dart';
+import 'package:demo/providers/player_provider.dart';
+import 'package:demo/screens/game_board_screen.dart';
+import 'package:demo/screens/match_history_screen.dart';
+import 'package:demo/screens/player_entry_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-//Developmenmt Branch///////
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
@@ -10,29 +21,26 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Initial Setup',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
-      home: const InitialSetupScreen(),
-    );
-  }
-}
-
-class InitialSetupScreen extends StatelessWidget {
-  const InitialSetupScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Initial Setup',
-          style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => GameProvider()),
+        ChangeNotifierProvider(create: (_) => PlayerProvider()),
+        ChangeNotifierProvider(create: (_) => ScoreProvider()),
+        ChangeNotifierProvider(create: (_) => HistoryProvider()),
+      ],
+      child: MaterialApp(
+        title: 'Tic Tac Toe',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+          useMaterial3: true,
         ),
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const PlayerEntryScreen(),
+          '/game': (context) => const GameBoardScreen(),
+          '/history': (context) => const MatchHistoryScreen(),
+        },
       ),
     );
   }
